9df330e1224b38f12eec2b64796661785bddd3ab
DanielsRT <robdaniels2001@gmail.com>
2023-03-24 15:06:16 -0500

    Implement account edit feature

--- passport-config.js
+++ passport-config.js
@@ -7,11 +7,10 @@ function initialize(passport, getUserByUsername, getUserById) {
         if (user == null) {
             return done(null, false, {message: 'No user with that username'});
         }
-        var hashedPassword = user.password;//JSON.stringify(user.password);
+        var hashedPassword = user.password;
         console.log('DB password: ' + hashedPassword)
         try {
             if (await bcrypt.compare(password, hashedPassword)) {
-            //if (password === user.password) {
                 return done(null, user);
             } else {
                 return done(null, false, {message: 'Password incorrect'});
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 const initializePassport = require('./passport-config');
 
-const users = database.execute("select * from users ", (err,result) => {
+var users = database.execute("select * from users ", (err,result) => {
     if (err) throw err;
     var users = JSON.parse(JSON.stringify(result));
     initializePassport(
@@ -60,10 +60,12 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
         var first_name = req.body.first_name;
         var last_name = req.body.last_name;
         var username = req.body.username;
+        var email = req.body.email;
+        var phone = req.body.phone;
         var isAdmin = false;
         var query = 
-        `INSERT INTO Users (username, password, last_name, first_name, isAdmin) VALUES
-        ("${username}","${hashedPassword}","${last_name}","${first_name}",${isAdmin})
+        `INSERT INTO Users (phone, email, username, password, last_name, first_name, isAdmin) VALUES
+        ("${phone}","${email}","${username}","${hashedPassword}","${last_name}","${first_name}",${isAdmin})
         `;
         database.execute(query, (err) => {
             if (err) throw err;
@@ -80,7 +82,45 @@ app.get('/account', checkAuthenticated, (req, res) => {
 });
 
 app.get('/account/edit', checkAuthenticated, (req, res) => {
-    res.render('account-edit.ejs',{user: req.user});
+    res.render('account-edit.ejs');
+});
+
+app.post('/account/edit', checkAuthenticated, (req, res) => {
+    try {
+        var first_name = req.body.first_name;
+        var last_name = req.body.last_name;
+        var username = req.body.username;
+        var phone = req.body.phone;
+        var email = req.body.email;
+        if(first_name != "") {
+            var query = `UPDATE users SET first_name = "${first_name}" WHERE user_id = ${req.user.user_id}`;
+            database.execute(query, (err) => {if (err) throw err;});
+            req.user.first_name = first_name;
+        }
+        if(last_name != "") {
+            var query = `UPDATE users SET last_name = "${last_name}" WHERE user_id = ${req.user.user_id}`;
+            database.execute(query, (err) => {if (err) throw err;});
+            req.user.last_name = last_name;
+        }
+        if(username != "") {
+            var query = `UPDATE users SET username = "${username}" WHERE user_id = ${req.user.user_id}`;
+            database.execute(query, (err) => {if (err) throw err;});
+            req.user.username = username;
+        }
+        if(email != "") {
+            var query = `UPDATE users SET email = "${email}" WHERE user_id = ${req.user.user_id}`;
+            database.execute(query, (err) => {if (err) throw err;});
+            req.user.email = email;
+        }
+        if(phone != "") {
+            var query = `UPDATE users SET phone = "${phone}" WHERE user_id = ${req.user.user_id}`;
+            database.execute(query, (err) => {if (err) throw err;});
+            req.user.phone = phone;
+        }
+        res.redirect('/account');
+    } catch {
+        res.redirect('/account');
+    }
 });
 
 app.delete('/logout', (req, res) => {
