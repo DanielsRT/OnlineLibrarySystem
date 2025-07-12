bc07a72f4c3866c946167cce33af2c810ee93a2d
Robert Daniels <robdaniels2001@gmail.com>
2025-07-12 02:44:57 -0400

    implement register page styling

--- server.js
+++ server.js
@@ -330,8 +330,8 @@ function checkNotAuthenticated(req, res, next) {
 }
 
 const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'librarymanagement.crt'))
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'localhost.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'localhost.crt'))
 };
 
 https.createServer(httpsOptions, app).listen(3000, function () {
